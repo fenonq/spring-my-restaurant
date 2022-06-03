060a2cba24c2f1bbecf06838ef670b2ac30ce869
fenonq <fenyk.ttt@gmail.com>
2022-06-04 00:37:27 +0300

    Initial commit

---
+++ src/main/java/com/fenonq/spring/myrestaurant/SpringMyRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.fenonq.spring.myrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMyRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMyRestaurantApplication.class, args);
+    }
+
+}

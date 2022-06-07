0708a27d7175288169de67d45b53f82bd79714dc
fenonq <fenyk.ttt@gmail.com>
2022-06-07 17:43:54 +0300

    added DataLoader

---
+++ src/main/java/com/fenonq/spring/myrestaurant/bootstrap/DataLoader.java
@@ -0,0 +1,85 @@
+package com.fenonq.spring.myrestaurant.bootstrap;
+
+import com.fenonq.spring.myrestaurant.model.*;
+import com.fenonq.spring.myrestaurant.model.enums.Locales;
+import com.fenonq.spring.myrestaurant.repositories.CategoryRepository;
+import com.fenonq.spring.myrestaurant.repositories.DishRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final DishRepository dishRepository;
+    private final CategoryRepository categoryRepository;
+
+    public DataLoader(DishRepository dishRepository, CategoryRepository categoryRepository) {
+        this.dishRepository = dishRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Dish dish =
+                Dish
+                        .builder()
+                        .price(123)
+                        .weight(321)
+                        .localizations(new HashMap<>())
+                        .build();
+
+        Category category =
+                Category
+                        .builder()
+                        .localizations(new HashMap<>())
+                        .dishes(new HashSet<>())
+                        .build();
+
+        LocalizedCategory localizedCategory1 =
+                LocalizedCategory
+                        .builder()
+                        .localizedId(new LocalizedId(Locales.EN.toString()))
+                        .name("EnCategory")
+                        .category(category)
+                        .build();
+        category.getLocalizations().put(Locales.EN.toString(), localizedCategory1);
+
+        LocalizedCategory localizedCategory2 =
+                LocalizedCategory
+                        .builder()
+                        .localizedId(new LocalizedId(Locales.UA.toString()))
+                        .name("UaCategory")
+                        .category(category)
+                        .build();
+        category.getLocalizations().put(Locales.UA.toString(), localizedCategory2);
+
+        LocalizedDish localizedDish1 =
+                LocalizedDish
+                        .builder()
+                        .localizedId(new LocalizedId(Locales.EN.toString()))
+                        .name("EnDish")
+                        .description("EnDesc")
+                        .dish(dish)
+                        .build();
+        dish.getLocalizations().put(Locales.EN.toString(), localizedDish1);
+
+        LocalizedDish localizedDish2 =
+                LocalizedDish
+                        .builder()
+                        .localizedId(new LocalizedId(Locales.UA.toString()))
+                        .name("UaDish")
+                        .description("UaDesc")
+                        .dish(dish)
+                        .build();
+        dish.getLocalizations().put(Locales.UA.toString(), localizedDish2);
+
+        category.getDishes().add(dish);
+        dish.setCategory(category);
+
+        categoryRepository.save(category);
+        dishRepository.save(dish);
+    }
+}

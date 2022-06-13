ceb4edcb13f9db050dabb8826b0ce81f30f4e62b
fenonq <fenyk.ttt@gmail.com>
2022-06-13 23:24:39 +0300

    added FileLoad

---
+++ src/main/java/com/fenonq/spring/myrestaurant/utils/FileUpload.java
@@ -0,0 +1,29 @@
+package com.fenonq.spring.myrestaurant.utils;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public abstract class FileUpload {
+
+    public static void saveFile(String fileName,
+                                MultipartFile multipartFile) throws IOException {
+        Path uploadPath = Paths.get("src/main/resources/static/img");
+
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            Path filePath = uploadPath.resolve(fileName);
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ioe) {
+            throw new IOException("Could not save image file: " + fileName, ioe);
+        }
+    }
+}
